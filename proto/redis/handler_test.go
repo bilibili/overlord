3c088704bc732b3a37a0d0de39d8cfced582b8d3
wayslog <wayslog@wayslog-mac-mini.local>
2018-06-05 15:16:59 +0800

    add Dial test of dial and close

--- proto/redis/handler_test.go
+++ proto/redis/handler_test.go
@@ -3,6 +3,12 @@ package redis
 import (
 	"testing"
 
+	"net"
+
+	"time"
+
+	"runtime"
+
 	"github.com/felixhao/overlord/proto"
 	"github.com/stretchr/testify/assert"
 )
@@ -15,8 +21,12 @@ func TestHandlerHandleClosed(t *testing.T) {
 		addr:    "127.0.0.1:7001",
 		conn:    conn,
 		buf:     newBuffer(conn, conn),
-		closed:  handlerClosed,
+		closed:  handlerOpening,
 	}
+	err := hd.Close()
+	assert.NoError(t, err)
+	assert.True(t, hd.Closed())
+	assert.NoError(t, hd.Close())
 
 	response, err := hd.Handle(&proto.Request{Type: proto.CacheTypeRedis})
 	assert.Error(t, err)
@@ -43,4 +53,34 @@ func TestHandlerHandleOk(t *testing.T) {
 	assert.True(t, ok)
 	assert.Equal(t, respString, protoResp.respObj.rtype)
 	assert.Equal(t, "OK", string(protoResp.respObj.data))
+}
+
+func _acceptClose(t *testing.T, fd net.Listener) {
+	sock, err := fd.Accept()
+	assert.NoError(t, err)
+	defer sock.Close()
+}
+
+func TestHandlerDialSockWithRandomPort(t *testing.T) {
+	fd, err := net.Listen("tcp", "0.0.0.0:0")
+	assert.NoError(t, err)
+	addr := fd.Addr()
+	go _acceptClose(t, fd)
+
+	// force to bind
+	runtime.Gosched()
+
+	dialFunc := Dial("mycluster", addr.String(), time.Second, time.Second, time.Second)
+	poolConn, err := dialFunc()
+	assert.NoError(t, err)
+
+	hd, ok := poolConn.(*handler)
+	assert.True(t, ok)
+
+	_, srcport, err := net.SplitHostPort(addr.String())
+	assert.NoError(t, err)
+	_, destport, err := net.SplitHostPort(hd.conn.RemoteAddr())
+	assert.NoError(t, err)
+
+	assert.Equal(t, srcport, destport)
 }
