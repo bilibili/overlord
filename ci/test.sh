44ddbdc06aefa7a800e2e8e308a8564a2bdeb5eb
wayslog <wayslog@wayslog-mac-mini.local>
2018-06-26 14:49:54 +0800

    tests: fixed of go cover

--- ci/test.sh
+++ ci/test.sh
@@ -8,7 +8,7 @@ echo "pwd is :$PTH"
 echo "Start to test overlord package"
 for pkg in `go list ./...|grep -v cmd`; do
     rm -f cover.out || echo "first try to remove"
-    go test --coverprofile=cover.out $pkg && go tool cover -func=cover.out
+    go test --coverprofile=cover.out ${pkg} && go tool cover -func=cover.out || echo "no test cases, skip"
 done
 # cd $PTH
 #find . -name "cover.out" | xargs -I{} cat {} >> proj.out
