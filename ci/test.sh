49643481151098b53cb786f1a4493cfb03310841
wayslog <wayslog@wayslog-mac-mini.local>
2018-06-26 14:37:38 +0800

    fixed of remove cd expression

--- ci/test.sh
+++ ci/test.sh
@@ -10,5 +10,5 @@ for pkg in `go list ./...|grep -v cmd`; do
     rm -f cover.out || echo "first try to remove"
     go test --coverprofile=cover.out $pkg && go tool cover -func=cover.out
 done
-cd $PTH
+# cd $PTH
 #find . -name "cover.out" | xargs -I{} cat {} >> proj.out
