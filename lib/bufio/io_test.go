a5c08128552556a807f7907a8dcb597c0739cd8c
wayslog <wayslog@163.com>
2018-07-09 19:45:33 +0800

    add TestWriterWriteOk of tests

--- lib/bufio/io_test.go
+++ lib/bufio/io_test.go
@@ -3,7 +3,10 @@ package bufio
 import (
 	"bytes"
 	"io"
+	"net"
+	libnet "overlord/lib/net"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -78,19 +81,70 @@ func TestReaderResetBuffer(t *testing.T) {
 	assert.Equal(t, io.EOF, err)
 }
 
-// func TestWriterWriteOk(t *testing.T) {
-// 	data := "Bilibili 干杯 - ( ゜- ゜)つロ"
-// 	buf := bytes.NewBuffer([]byte{})
-// 	w := NewWriter(buf)
-// 	err := w.Write([]byte(data))
-// 	assert.NoError(t, err)
+type mockAddr string
 
-// 	err = w.Write([]byte(data))
-// 	assert.NoError(t, err)
+func (m mockAddr) Network() string {
+	return "tcp"
+}
+func (m mockAddr) String() string {
+	return string(m)
+}
 
-// 	err = w.WriteString(data)
-// 	assert.NoError(t, err)
+type mockConn struct {
+	rbuf *bytes.Buffer
+	wbuf *bytes.Buffer
+	addr mockAddr
+}
 
-// 	err = w.Flush()
-// 	assert.NoError(t, err)
-// }
+func (m *mockConn) Read(b []byte) (n int, err error) {
+	return m.rbuf.Read(b)
+}
+func (m *mockConn) Write(b []byte) (n int, err error) {
+	return m.wbuf.Write(b)
+}
+
+// writeBuffers impl the net.buffersWriter to support writev
+func (m *mockConn) writeBuffers(buf *net.Buffers) (int64, error) {
+	return buf.WriteTo(m.wbuf)
+}
+
+func (m *mockConn) Close() error         { return nil }
+func (m *mockConn) LocalAddr() net.Addr  { return m.addr }
+func (m *mockConn) RemoteAddr() net.Addr { return m.addr }
+
+func (m *mockConn) SetDeadline(t time.Time) error      { return nil }
+func (m *mockConn) SetReadDeadline(t time.Time) error  { return nil }
+func (m *mockConn) SetWriteDeadline(t time.Time) error { return nil }
+
+// _createConn is useful tools for handler test
+func _createConn(data []byte) *libnet.Conn {
+	return _createRepeatConn(data, 1)
+}
+
+func _createRepeatConn(data []byte, r int) *libnet.Conn {
+	mconn := &mockConn{
+		addr: "127.0.0.1:12345",
+		rbuf: bytes.NewBuffer(bytes.Repeat(data, r)),
+		wbuf: new(bytes.Buffer),
+	}
+	conn := libnet.NewConn(mconn, time.Second, time.Second)
+	return conn
+}
+
+func TestWriterWriteOk(t *testing.T) {
+	data := "Bilibili 干杯 - ( ゜- ゜)つロ"
+
+	conn := _createConn(nil)
+	w := NewWriter(conn)
+	err := w.Write([]byte(data))
+	assert.NoError(t, err)
+
+	err = w.Write([]byte(data))
+	assert.NoError(t, err)
+
+	err = w.WriteString(data)
+	assert.NoError(t, err)
+
+	err = w.Flush()
+	assert.NoError(t, err)
+}
