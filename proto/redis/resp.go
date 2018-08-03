648df34f1d6f0ea00362fea95221ba71627b8431
Tanghui Lin <xmutanghui@gmail.com>
2018-08-03 14:40:35 +0800

    reset in need

--- proto/redis/resp.go
+++ proto/redis/resp.go
@@ -43,9 +43,9 @@ func (r *resp) reset() {
 	r.rTp = respUnknown
 	r.data = nil
 	r.arrayn = 0
-	for _, ar := range r.array {
-		ar.reset()
-	}
+	// for _, ar := range r.array {
+	// 	ar.reset()
+	// }
 }
 
 func (r *resp) copy(re *resp) {
