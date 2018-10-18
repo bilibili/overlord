a50f5099d1ec03fd46c627b4c2dbf3675690a8ba
wayslog <wayslog@163.com>
2018-10-18 19:00:23 +0800

    add benchmark test for request IsSupport

--- proto/redis/request_test.go
+++ proto/redis/request_test.go
@@ -23,3 +23,46 @@ func TestRequestNewRequest(t *testing.T) {
 	assert.Equal(t, []byte("LLEN"), req.Cmd())
 	assert.Equal(t, "mylist", string(req.Key()))
 }
+
+
+var cmds = []string{
+	"4\r\nDUMP", "6\r\nEXISTS", "4\r\nPTTL",
+	"6\r\nSELECT", "4\r\nTIME", "6\r\nCONFIG",
+	"4\r\nHSET", "6\r\nHSETNX", "7\r\nLINSERT",
+	"4\r\nQUIT",
+}
+
+func str2Request(key string) *Request {
+	r := &Request{
+		resp: &resp{
+			rTp:    respArray,
+			data:   []byte("1"),
+			arrayn: 1,
+			array: []*resp{
+				&resp{
+					rTp:    respBulk,
+					data:   []byte(key),
+					array:  nil,
+					arrayn: 0,
+				},
+			},
+		},
+		reply: nil,
+	}
+	return r
+}
+
+func BenchmarkRequestIsSupportByMap(b *testing.B) {
+	b.StopTimer()
+	reqs := make([]*Request, len(cmds))
+	for i, key := range cmds {
+		reqs[i] = str2Request(key)
+	}
+	b.StartTimer()
+
+	for i := 0; i < 1000000; i++ {
+		for _, req := range reqs {
+			req.IsSupport()
+		}
+	}
+}
