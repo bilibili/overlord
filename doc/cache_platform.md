f26a82d22ba4970a369dc4819680e1e3c6b63007
wayslog <wayslog@163.com>
2018-10-31 17:24:13 +0800

    add create executor implmented

--- doc/cache_platform.md
+++ doc/cache_platform.md
@@ -52,3 +52,38 @@ overlord
 7. event为running时，则表示任务启动成功并且正常运行
 8. event为failed时，scheduler 需要重新调度重新分配task
 9. 当running数为task需要部署的instance数时，表示所有任务部署成功，scheduler更新task状态为完成并写入将状态写入ectd
+
+## Executor 约定
+
+### 注意
+
+无论是服务，还是缓存配置还是目录，始终用 memcache 指代 memcache 服务。仅在表示 memcached 这个 binary 本身的时候使用 memcached
+
+### 目录约定
+一些约定：
+
+1. redis server binary 的存放路径为 /data/lib/redis/4.0.8/bin/redis-server
+2. memcache 的存放路径为 /data/lib/memcache/1.5.10/bin/memcached
+3. 多版本共存请修改 4.0.8(1.5.10) 到对应版本
+4. redis 的 working dir 位置 /data/redis[_cluster]/${port}
+5. redis 节点的配置文件约定为 /data/redis/${port}/redis.conf
+6. redis_cluster 节点的nodes.conf文件约定为 /data/redis_cluster/${port}/nodes.conf
+7. redis 的 pid 文件为 redis.pid 放在 working dir 目录下。
+8. memcache 类似。
+9. 所有缓存服务都需要在 working dir 下维持一个 meta.toml 文件，用来存储类似 "cluster name"等节点信息。严格禁止并发修改这个文件。
+10. mesos 的 executor 的 tmp dir 目录为 /data/var/overlord/tmp/{task_id}/
+
+所有文件（目录）的权限均为 755，用户恒为 root.root
+
+
+一个典型的 redis 目录为：
+
+```
+7010
+├── console.log # 启动日志
+├── meta.tmol   # 元信息，里面保存了启动关闭命令
+├── nodes.conf  # 仅存在于 redis cluster 的 nodes.conf 文件
+├── redis.conf  # redis 配置文件
+└── redis.pid   # redis 的 pid 
+```
+>>>>>>> add create executor implmented
---
+++ task/create/exec.go
@@ -0,0 +1,148 @@
+// Package create is the package of create and make Ken happy.
+package create
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"overlord/lib/dir"
+	"overlord/lib/log"
+	"path/filepath"
+
+	"overlord/config"
+
+	"github.com/BurntSushi/toml"
+)
+
+func getDefaultServiceWorkDir() string {
+	if config.GetRunMode() != config.RunModeTest {
+		return "/data/%s/%d"
+	}
+
+	return "./tmp/data/%s/%d"
+}
+
+// DeployInfo is the struct to communicate between etcd and executor
+// must be serialized and deserialized by json
+type DeployInfo struct {
+	// TaskID is the id of task
+	TaskID string
+
+	CacheType string
+	Port      int
+
+	// for golang executor to spawn that
+	ExecStart   string
+	ExecStop    string
+	ExecRestart string
+
+	// TplTree is the Tree which contains a key as path of the file,
+	// and value as the content of the file.
+	TplTree map[string]string
+}
+
+func renderTplTree(tplTree map[string]string) (err error) {
+	var (
+		basename string
+		abs      string
+	)
+
+	for path, content := range tplTree {
+		basename, err = dir.GetAbsDir(path)
+		if err != nil {
+			return
+		}
+		err = dir.MkDirAll(basename)
+		if err != nil {
+			return
+		}
+		abs, err = filepath.Abs(path)
+		if err != nil {
+			return
+		}
+
+		err = ioutil.WriteFile(abs, []byte(content), 0755)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func renderMetaIntoFile(workdir string, di *DeployInfo) error {
+	file := fmt.Sprintf("%s/meta.toml", workdir)
+	fd, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	err = fd.Chmod(0755)
+	if err != nil {
+		return err
+	}
+	encoder := toml.NewEncoder(fd)
+
+	return encoder.Encode(di)
+}
+
+func shlex(cmd string) (command string, args []string) {
+	return "bash", []string{"-c", `"` + cmd + `"`}
+}
+
+func outputIntoFile(workdir string, data []byte) error {
+	console := fmt.Sprintf("%s/console.log", workdir)
+	return ioutil.WriteFile(console, data, 0755)
+}
+
+// SetupCacheService will create new cache service
+func SetupCacheService(info *DeployInfo) error {
+
+	// 1. render template tree into the path
+	//   1.1 foreach fpath, content in TplTree
+	//   1.4 mkdir for fpath's basedir
+	//   1.3 write content into
+	err := renderTplTree(info.TplTree)
+	if err != nil {
+		log.Warnf("error when render template tree")
+		return err
+	}
+
+	// 2. execute given command
+	//   2.0 mk working dir
+	workdir := fmt.Sprintf(getDefaultServiceWorkDir(), info.CacheType, info.Port)
+	err = dir.MkDirAll(workdir)
+	if err != nil {
+		log.Errorf("fail to create working dir")
+		return err
+	}
+	err = renderMetaIntoFile(workdir, info)
+	if err != nil {
+		log.Errorf("fail to create meta data file due to %s", err)
+		return err
+	}
+
+	//   2.1 spawn new executor with given ExecStart
+	//   2.2 NOTICE: all the cache progress must be working with cache
+	//   2.3 anyway defer p.Wait() wait for service is started.
+	command, args := shlex(info.ExecStart)
+
+	cmd := exec.Command(command, args...)
+	cmd.Dir = workdir
+
+	// must wait for remove defunc progress
+	defer func() {
+		err := cmd.Wait()
+		if err != nil {
+			log.Warnf("spawn wait sub command fail due to %s", err)
+		}
+	}()
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Errorf("fail to create output file %s", err)
+		return err
+	}
+
+	return outputIntoFile(workdir, output)
+}
