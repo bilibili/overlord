3882343aaa1f3a9984b3e93a1696f6c5fbc45987
wayslog <wayslog@163.com>
2018-11-20 17:24:53 +0800

    add many apis for cluster job api

--- api/server/approve.go
+++ api/server/approve.go
@@ -1,6 +1,23 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func approveJob(c *gin.Context) {
+	id := c.PostForm("job_id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest, "job_id not exists")
+		return
+	}
+
+	err := svc.ApproveJob(id)
+	if err != nil {
+		eJSON(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]string{"job": "approved"})
 }
--- api/service/cluster.go
+++ api/service/cluster.go
@@ -20,8 +20,15 @@ func (s *Service) GetClusters() ([]*model.Cluster, error) {
 	return s.d.GetClusters(context.Background())
 }
 
-
 // RemoveCluster will remove the cluster if the cluster is not associated with other appids
-func (s *Service) RemoveCluster(cname string) (*model.Cluster, error) {
-	return nil, nil
+func (s *Service) RemoveCluster(cname string) (string, error) {
+	return s.d.RemoveCluster(context.Background(), cname)
+}
+
+// ScaleCluster will scale the given cluster with params.
+// for redis cluster, number means scale chunk.
+// for redis/memcache, number means scale numbers.
+func (s *Service) ScaleCluster(p *model.ParamScale) (jobID string,err error) {
+	jobID, err = s.d.ScaleCluster(context.Background(), p)
+	return
 }
