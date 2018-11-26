b4cf0bde1fd5916c916f090ec04df846b6e54cee
Tanghui Lin <xmutanghui@gmail.com>
2018-11-26 14:59:25 +0800

    add deploy shell

--- doc/deploy.md
+++ doc/deploy.md
@@ -1,7 +1,8 @@
 ## 部署说明
 
 ### 安装jdk 
-mesos及zookeeper依赖java,必须使用jdk8以上
+mesos及zookeeper依赖java,必须使用jdk8以上  
+[安装jdk8](../scripts/install_java.sh)
 
 ### 安装mesos 
 建议版本 v1.7.0,建议部署三master以保证高可用  
@@ -12,7 +13,9 @@ mesos及zookeeper依赖java,必须使用jdk8以上
 用于存储部署信息及集群原信息，推荐版本v3.3.10
 
 ### 安装zookeeper
-可选，如果mesos-master需要保证高可用，则为必选，推荐版本v3.4.12
+可选，如果mesos-master需要保证高可用，则为必选，推荐版本v3.4.12  
+[安装zk集群](../scripts/install_zk.sh)
+** 需要把ip_array替换为部署的机器列表地址**
 
 ### 安装ngnix 
 可选，用于作为文件服务器，提供redis memcache mesos-executor的二进制下载
---
+++ scripts/install_zk.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+if [ ! -n "$1" ];then
+echo "zookeeper myid can not be null,pass myid by arg.
+eg:./install_zk.sh 2"
+exit 1
+else
+myid=$1
+fi
+echo $myid
+zk_ver=3.4.12
+ip_array=("host1" "host2" "host3")
+ip_num=${#ip_array[@]}
+jdk=/usr/local/jdk8
+zk_path=/usr/local/zookeeper-${zk_ver}
+work_dir=/data/server/zookeeper
+log_dir=/data/log/zookeeper
+curl -L http://apache.stu.edu.tw/zookeeper/zookeeper-{zk_ver}/zookeeper-3.4.12.tar.gz -o zookeeper-${zk_ver}.tar.gz
+tar zxf zookeeper-${zk_ver}.tar.gz
+mkdir -p $zk_path
+mkdir -p $log_dir
+mkdir -p $work_dir
+echo $myid>$work_dir/myid
+mv zookeeper-${zk_ver}/* $zk_path
+echo "tickTime=2000" > $zk_path/conf/zoo.cfg
+echo "initLimit=10">> $zk_path/conf/zoo.cfg 
+echo "syncLimit=5" >>$zk_path/conf/zoo.cfg
+echo "dataDir=/data/server/zookeeper" >>$zk_path/conf/zoo.cfg
+echo "clientPort=2181" >>$zk_path/conf/zoo.cfg
+echo "autopurge.snapRetainCount=5" >>$zk_path/conf/zoo.cfg
+echo "autopurge.purgeInterval=24" >>$zk_path/conf/zoo.cfg
+for ((index=0;index<$ip_num;index++))
+do
+    tmp=$[$index+1]
+    echo "server.$tmp=${ip_array[index]}:2888:3888" >>$zk_path/conf/zoo.cfg 
+done
+
+### start by supervisor
+apt-get install -y supervisor
+supervisor_path=/etc/supervisor/conf.d/zookeeper.conf
+echo "[program:zookeeper]">$supervisor_path
+echo "command=/usr/local/zookeeper-${zk_ver}/bin/zkServer.sh start-foreground">>$supervisor_path
+echo "directory=/usr/local/zookeeper-${zk_ver}">>$supervisor_path
+echo 'user=root
+autostart=true
+autorestart=true
+stopsignal=KILL
+startsecs=10
+startretries=3
+stdout_logfile = /data/log/zookeeper/stdout.log
+stdout_logfile_backups = 3
+stderr_logfile = /data/log/zookeeper/stderr.log
+stderr_logfile_backups = 3
+logfile_maxbytes=20MB'>>$supervisor_path
+echo "environment=JAVA_HOME=\"${jdk}\",JRE_HOME=\"${jdk}\"/jre\"">>$supervisor_path
+supervisorctl update
