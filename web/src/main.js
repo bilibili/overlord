2e2775fff842beca8fbc9ea8df55454c752c4c62
KarenChuang <magic_gua@163.com>
2018-11-29 23:38:50 +0800

    ui: init

---
+++ web/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI, { size: 'small', zIndex: 3000 })
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
