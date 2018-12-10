35996ebfe05b112dd4f87b88b1f1bc706504c73a
wayslog <wayslog@163.com>
2018-12-10 17:04:32 +0800

    fixed for apiserver and add setup-etcd.sh

---
+++ scripts/install/setupetcd.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+/data/etcd/etcdctl mkdir /overlord/clusters
+/data/etcd/etcdctl mkdir /ovelord/instances
+/data/etcd/etcdctl mkdir /overlord/heartbeat
+/data/etcd/etcdctl mkdir /overlord/config
+/data/etcd/etcdctl mkdir /overlord/jobs
+/data/etcd/etcdctl mkdir /overlord/job_detail
+/data/etcd/etcdctl mkdir /overlord/framework
+/data/etcd/etcdctl mkdir /overlord/appids
+/data/etcd/etcdctl mkdir /overlord/specs
