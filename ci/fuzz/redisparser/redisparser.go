f26aac0a0b9007001d43d0d4d0abc34051310cb2
wayslog <zxs867179@gmail.com>
2019-04-08 15:58:29 +0800

    add run-fuzz.sh scripts

--- ci/fuzz/redisparser/redisparser.go
+++ ci/fuzz/redisparser/redisparser.go
@@ -1,4 +1,4 @@
-package rparser
+package redisparser
 
 import (
 	"bytes"
---
+++ scripts/run-fuzz.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export GO111MODULE=on
+
+# generate vendor clean go mod files
+go mod vendor
+mv go.mod go.mod.backup
+mv go.sum go.sum.backup
+
+# unset go11module
+export GO111MODULE=auto
+
+#ls ci/fuzz/ | xargs -n1 -I"{}" cd {} && go-fuzz-build
+ls ci/fuzz/ | xargs -n1 -I"{}" python scripts/fuzz_tools.py ci/fuzz/{}
+
+# defer setting
+rm -rf vendor
+mv go.mod.backup go.mod
+mv go.sum.backup go.sum
