a1beb101f1ada6af9c112cd4f6e22daf5db62ef3
gutan <iamyumingxing@gmail.com>
2019-11-27 11:25:03 +0800

    fix:when cursor < 0 && cursor > ^int32(^uint32(0)>>1),idx < 0

--- proxy/slowlog/slowlog.go
+++ proxy/slowlog/slowlog.go
@@ -21,7 +21,7 @@ func newStore(name string) *Store {
 // Store is the collector of slowlog
 type Store struct {
 	name   string
-	cursor int32
+	cursor uint32
 	msgs   []atomic.Value
 }
 
@@ -32,7 +32,7 @@ func (s *Store) Record(msg *proto.SlowlogEntry) {
 	}
 
 	for {
-		if atomic.CompareAndSwapInt32(&s.cursor, s.cursor, s.cursor+1) {
+		if atomic.CompareAndSwapUint32(&s.cursor, s.cursor, s.cursor+1) {
 			idx := s.cursor % slowlogMaxCount
 			s.msgs[idx].Store(msg)
 			if fh != nil {
